387cce1ccec9ae7dbb63acf9dfbacac0fb766ae0
Mariusz Marzec <mariusz.marzec@miquido.com>
2020-01-13 21:40:01 +0100

    init

---
+++ common/src/mingwMain/kotlin/sample/SampleMingw.kt
@@ -0,0 +1,13 @@
+package sample
+
+actual class Sample {
+    actual fun checkMe() = 7
+}
+
+actual object Platform {
+    actual val name: String = "Native"
+}
+
+fun main() {
+    println(hello())
+}
