258c7ae07e136992cc60ac055b66c015c6eca0ef
Mariusz Marzec <mariusz.marzec00@gmail.com>
2020-11-23 16:50:05 +0100

    Equipment table added

--- common/src/jvmMain/kotlin/com/marzec/database/EquipmentTable.kt
+++ common/src/jvmMain/kotlin/com/marzec/database/EquipmentTable.kt
@@ -1,13 +1,13 @@
 package com.marzec.database
 
-import com.marzec.model.domain.Category
+import com.marzec.model.domain.Equipment
 import org.jetbrains.exposed.dao.Entity
 import org.jetbrains.exposed.dao.EntityClass
 import org.jetbrains.exposed.dao.id.EntityID
 import org.jetbrains.exposed.dao.id.IdTable
 import org.jetbrains.exposed.sql.Column
 
-object CategoryTable : IdTable<String>("categories") {
+object EquipmentTable : IdTable<String>("equipment") {
 
     override val id: Column<EntityID<String>> = varchar("id", 36).entityId()
 
@@ -16,10 +16,10 @@ object CategoryTable : IdTable<String>("categories") {
     override val primaryKey by lazy { super.primaryKey ?: PrimaryKey(id) }
 }
 
-class CategoryEntity(id: EntityID<String>): Entity<String>(id) {
+class EquipmentEntity(id: EntityID<String>): Entity<String>(id) {
     var name by CategoryTable.name
 
-    fun toDomain() = Category(id.value, name)
+    fun toDomain() = Equipment(id.value, name)
 
-    companion object : EntityClass<String, CategoryEntity>(CategoryTable)
+    companion object : EntityClass<String, EquipmentEntity>(EquipmentTable)
 }
