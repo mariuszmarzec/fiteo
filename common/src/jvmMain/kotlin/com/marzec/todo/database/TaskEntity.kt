e827abe6161fe0e8c930d4fd62246af3e83f14f9
Mariusz Marzec <mariusz.marzec00@gmail.com>
2020-11-29 18:51:58 +0100

    Added lacking user id field

--- common/src/jvmMain/kotlin/com/marzec/todo/database/TaskEntity.kt
+++ common/src/jvmMain/kotlin/com/marzec/todo/database/TaskEntity.kt
@@ -1,11 +1,13 @@
 package com.marzec.todo.database
 
+import com.marzec.database.UserTable
+import java.time.LocalDateTime
 import org.jetbrains.exposed.dao.IntEntity
 import org.jetbrains.exposed.dao.IntEntityClass
 import org.jetbrains.exposed.dao.id.EntityID
 import org.jetbrains.exposed.dao.id.IntIdTable
+import org.jetbrains.exposed.sql.ReferenceOption
 import org.jetbrains.exposed.sql.`java-time`.datetime
-import java.time.LocalDateTime
 
 object TasksTable : IntIdTable("todo_tasks") {
     val description = text("description")
@@ -13,6 +15,7 @@ object TasksTable : IntIdTable("todo_tasks") {
     val modifiedTime = datetime("modified_time").default(LocalDateTime.now())
     val isToDo = bool("is_to_do")
     val priority = integer("priority")
+    val userId = reference("user_id", UserTable, onDelete = ReferenceOption.CASCADE)
 }
 
 class TaskEntity(id: EntityID<Int>) : IntEntity(id) {
@@ -23,6 +26,7 @@ class TaskEntity(id: EntityID<Int>) : IntEntity(id) {
     var isToDo by TasksTable.isToDo
     var priority by TasksTable.priority
     var subtasks by TaskEntity.via(TaskToSubtasksTable.parent, TaskToSubtasksTable.child)
+    val user by via(UserTable)
 
     companion object : IntEntityClass<TaskEntity>(TasksTable)
 }
--- common/src/jvmMain/kotlin/com/marzec/todo/database/ToDoListEntity.kt
+++ common/src/jvmMain/kotlin/com/marzec/todo/database/ToDoListEntity.kt
@@ -6,9 +6,11 @@ import org.jetbrains.exposed.dao.IntEntity
 import org.jetbrains.exposed.dao.IntEntityClass
 import org.jetbrains.exposed.dao.id.EntityID
 import org.jetbrains.exposed.dao.id.IntIdTable
+import org.jetbrains.exposed.sql.ReferenceOption
 
 object ToDoListTable : IntIdTable("todo_lists") {
     val title = varchar("title", 100)
+    val userId = reference("user_id", UserTable, onDelete = ReferenceOption.CASCADE)
 }
 
 object ToDoListsToTasksTable : IntIdTable("todo_lists_to_tasks") {
@@ -19,6 +21,7 @@ object ToDoListsToTasksTable : IntIdTable("todo_lists_to_tasks") {
 class ToDoListEntity(id: EntityID<Int>) : IntEntity(id) {
     var title by ToDoListTable.title
     var tasks by via(ToDoListsToTasksTable.toDoList, ToDoListsToTasksTable.task)
+    val user by via(UserTable)
 
     companion object : IntEntityClass<UserEntity>(UserTable)
 }
