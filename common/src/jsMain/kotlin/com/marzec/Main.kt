02526bd6d9f828afbb60a3152d3744987f538188
Mariusz Marzec <mariusz.marzec00@gmail.com>
2020-12-19 21:05:21 +0100

    temporary mvi solution

--- common/src/jsMain/kotlin/com/marzec/Main.kt
+++ common/src/jsMain/kotlin/com/marzec/Main.kt
@@ -6,21 +6,24 @@ import com.marzec.model.dto.toDomain
 import io.ktor.client.request.get
 import kotlinx.browser.document
 import kotlinx.browser.window
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
-import kotlinx.html.h1
 import react.RProps
 import react.child
 import react.dom.div
 import react.dom.h1
 import react.dom.h3
 import react.dom.img
-import react.dom.li
 import react.dom.render
 import react.dom.ul
 import react.functionalComponent
 import react.useEffect
 import react.useState
+
 
 val endpoint = window.location.origin
 
@@ -28,6 +31,7 @@ suspend fun getExercises(): List<Exercise> =
         jsonClient.get<List<ExerciseDto>>(endpoint + ApiPath.EXERCISES)
                 .map { it.toDomain() }
 
+@ExperimentalCoroutinesApi
 fun main() {
     render(document.getElementById("root")) {
         child(App)
@@ -36,23 +40,38 @@ fun main() {
 
 private val scope = MainScope()
 
+@ExperimentalCoroutinesApi
 val App = functionalComponent<RProps> { _ ->
-    val (exercises, setExercises) = useState(emptyList<Exercise>())
+    val viewModel = ExerciseListViewModel()
+    val (state, setState) = useState<State<List<Exercise>>>(State.Loading(emptyList()))
 
-    useEffect(dependencies = listOf()) {
+    useEffect(emptyList()) {
         scope.launch {
-            setExercises(getExercises())
+            viewModel.sendAction(ExercisesListActions.Initialization)
+            viewModel.state.collect {
+                setState(it)
+            }
         }
     }
 
-    ul {
-        exercises.groupBy { it.category }.forEach { (categories, exercises) ->
-            h1 { +categories.fold("") { acc, value -> "$acc${value.name} " } }
-            exercises.forEach { exercise ->
-                child(ExercisesView) {
-                    this.attrs.exercise = exercise
+    when (state) {
+        is State.Data -> {
+            ul {
+                state.data.groupBy { it.category }.forEach { (categories, exercises) ->
+                    h1 { +categories.fold("") { acc, value -> "$acc${value.name} " } }
+                    exercises.forEach { exercise ->
+                        child(ExercisesView) {
+                            this.attrs.exercise = exercise
+                        }
+                    }
                 }
             }
+        }
+        is State.Loading -> {
+            h3 { +"Loading" }
+        }
+        is State.Error -> {
+            h3 { +"Error: ${state.message}" }
         }
     }
 }
@@ -68,4 +87,71 @@ val ExercisesView = functionalComponent<ExercisesViewProps> { props ->
 
 external interface ExercisesViewProps : RProps {
     var exercise: Exercise
+}
+
+sealed class State<T> {
+
+    data class Data<T>(val data: T) : State<T>()
+    data class Loading<T>(val data: T?) : State<T>()
+    data class Error<T>(val message: String) : State<T>()
+}
+
+sealed class Resource<T> {
+
+    data class Content<T>(val data: T) : Resource<T>()
+    data class Error<T>(val message: String) : Resource<T>()
+}
+
+@ExperimentalCoroutinesApi
+open class ViewModel<Type, Action>(defaultState: State<Type>) {
+
+    private val viewModelScope = MainScope()
+
+    var intents = mapOf<Action, Intent<Action, State<Type>>>()
+
+    private val _state = MutableStateFlow(defaultState)
+
+    val state: Flow<State<Type>>
+        get() = _state
+
+    fun sendAction(action: Action) {
+        viewModelScope.launch {
+            val intent = intents[action]
+            console.log(intent)
+            requireNotNull(intent)
+
+            val result = intent.onTrigger.invoke()
+            console.log(result)
+
+            val newState = intent.reducer(result, _state.value)
+
+            _state.value = newState
+        }
+    }
+}
+
+sealed class ExercisesListActions {
+    object Initialization : ExercisesListActions()
+}
+
+data class Intent<Action, State>(
+        val onTrigger: suspend () -> Any?,
+        val reducer: suspend (Any?, State) -> State
+)
+
+@ExperimentalCoroutinesApi
+class ExerciseListViewModel : ViewModel<List<Exercise>, ExercisesListActions>(State.Loading(emptyList())) {
+
+    init {
+        intents = mapOf(
+                ExercisesListActions.Initialization to Intent(
+                        onTrigger = {
+                            getExercises()
+                        },
+                        reducer = { actionResult: Any?, state: State<List<Exercise>> ->
+                            (actionResult as? List<Exercise>)?.let { State.Data(it) } ?: State.Error("Data loading error")
+                        }
+                )
+        )
+    }
 }
