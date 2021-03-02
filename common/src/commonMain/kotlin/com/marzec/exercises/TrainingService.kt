034f00b46217c7e7a62e2ee32f644df9d4097121
Mariusz Marzec <mariusz.marzec00@gmail.com>
2021-03-02 22:01:33 +0100

    Added creating training from template

--- common/src/commonMain/kotlin/com/marzec/exercises/TrainingService.kt
+++ common/src/commonMain/kotlin/com/marzec/exercises/TrainingService.kt
@@ -1,11 +1,8 @@
 package com.marzec.exercises
 
-import com.marzec.model.domain.CreateTraining
-import com.marzec.model.domain.CreateTrainingTemplate
-import com.marzec.model.domain.Training
-import com.marzec.model.domain.TrainingTemplate
-import com.marzec.repositories.TrainingRepository
-import com.marzec.repositories.TrainingTemplateRepository
+import com.marzec.model.domain.*
+import com.marzec.repositories.*
+import kotlinx.datetime.*
 
 interface TrainingService {
     fun getTrainingTemplates(userId: Int): List<TrainingTemplate>
@@ -29,7 +26,10 @@ interface TrainingService {
 
 class TrainingServiceImpl(
         private val templateRepository: TrainingTemplateRepository,
-        private val trainingRepository: TrainingRepository
+        private val trainingRepository: TrainingRepository,
+        private val exercisesRepository: ExercisesRepository,
+        private val categoriesRepository: CategoriesRepository,
+        private val equipmentRepository: EquipmentRepository
 ) : TrainingService {
     override fun getTrainingTemplates(userId: Int): List<TrainingTemplate> = templateRepository.getTemplates(userId)
 
@@ -42,8 +42,30 @@ class TrainingServiceImpl(
     override fun removeTrainingTemplate(userId: Int, trainingTemplateId: Int): TrainingTemplate =
             templateRepository.removeTemplate(userId, trainingTemplateId)
 
-    override fun createTraining(userId: Int, templateId: Int): Training =
-            trainingRepository.createTraining(userId, templateId)
+    override fun createTraining(userId: Int, templateId: Int): Training {
+        val newTraining = trainingRepository.createTraining(userId, templateId)
+        val template = templateRepository.getTemplate(userId, templateId)
+        val allExercises = exercisesRepository.getAll().filter { exercise -> exercise.neededEquipment.all { equipment -> equipment in template.availableEquipment } }
+
+        val trainingUpdate = CreateTraining(
+                Clock.System.now().toLocalDateTime(TimeZone.UTC),
+                exercisesWithProgress = template.exercises.map { trainingPart ->
+                    val exercise = allExercises.filter { exercise ->
+                        exercise.category.all { it in trainingPart.categories } &&
+                                exercise.id !in trainingPart.excludedExercises &&
+                                exercise.neededEquipment.none { it in trainingPart.excludedEquipment }
+                    }.randomOrNull()
+                            ?: trainingPart.pinnedExercise
+                            ?: throw NoSuchElementException("No exercise for training part with: ${trainingPart.id}")
+                    CreateTrainingExerciseWithProgress(exercise.id, emptyList())
+                }
+        )
+        return trainingRepository.updateTraining(
+                userId = userId,
+                trainingId = newTraining.id,
+                training = trainingUpdate
+        )
+    }
 
     override fun getTraining(userId: Int, trainingId: Int): Training =
             trainingRepository.getTraining(userId, trainingId)
