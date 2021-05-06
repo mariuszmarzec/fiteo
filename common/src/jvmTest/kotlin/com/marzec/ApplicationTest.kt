835cd4148e28b2df09c1d1d30ae19f6a40b1ce2a
Mariusz Marzec <mariusz.marzec00@gmail.com>
2021-05-06 16:54:24 +0200

    New test helpers for running test

--- common/src/jvmTest/kotlin/com/marzec/ApplicationTest.kt
+++ common/src/jvmTest/kotlin/com/marzec/ApplicationTest.kt
@@ -1,21 +1,16 @@
 package com.marzec
 
-import com.marzec.data.InitialDataLoader
-import com.marzec.di.MainModule
 import com.marzec.io.ExercisesReader
 import com.marzec.io.ResourceFileReader
 import com.marzec.model.dto.ExercisesFileDto
+import io.ktor.http.HttpMethod
+import io.ktor.http.HttpStatusCode
+import io.ktor.server.testing.handleRequest
+import io.mockk.every
+import junit.framework.Assert.assertEquals
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
-import io.ktor.server.testing.*
-import io.ktor.http.*
-import io.mockk.every
-import io.mockk.mockk
-import junit.framework.Assert.assertEquals
-import org.koin.core.context.loadKoinModules
-import org.koin.dsl.module
-import org.koin.test.AutoCloseKoinTest
 
 class ApplicationTest {
 
@@ -26,21 +21,17 @@ class ApplicationTest {
 
     @Test
     fun testContainer() {
-        withTestApplication({
-            module(MainModule.plus(module {
-                factory<ResourceFileReader>(override = true) { val mockk = mockk<ResourceFileReader>()
+        withMockTestApplication(
+            mockConfiguration = {
+                factoryMock<ResourceFileReader> { mockk ->
                     every { mockk.read(any()) } returns ""
-                    mockk
                 }
-                factory<ExercisesReader>(override = true) { val mockk = mockk<ExercisesReader>()
+                factoryMock<ExercisesReader> { mockk ->
                     every { mockk.parse(any()) } returns ExercisesFileDto(
                         null, null, null
                     )
-                    mockk
                 }
-            })
-            )
-        }) {
+            }) {
             handleRequest(HttpMethod.Get, ApiPath.EQUIPMENT).apply {
                 assertEquals("DZIALA1", response.content)
                 assertEquals(HttpStatusCode.OK, response.status())
@@ -52,4 +43,3 @@ class ApplicationTest {
     fun cleanUp() {
     }
 }
-
--- common/src/jvmTest/kotlin/com/marzec/TestHelpers.kt
+++ common/src/jvmTest/kotlin/com/marzec/TestHelpers.kt
@@ -1,7 +1,14 @@
 package com.marzec
 
 import com.marzec.database.DbSettings
+import com.marzec.di.MainModule
+import io.ktor.application.Application
+import io.ktor.server.testing.TestApplicationEngine
+import io.ktor.server.testing.withTestApplication
+import io.mockk.mockk
 import org.flywaydb.core.Flyway
+import org.koin.core.module.Module
+import org.koin.dsl.module
 
 fun setupDb() {
     DbSettings.dbEndpoint = "jdbc:mysql://localhost:3306/fiteo_test_database?createDatabaseIfNotExist=TRUE"
@@ -12,4 +19,19 @@ fun setupDb() {
 
     flyway.clean()
     flyway.migrate()
+}
+
+fun <T> withMockTestApplication(
+    mockConfiguration: Module.() -> Unit,
+    applicationModule: Application.(List<Module>) -> Unit = Application::module,
+    test: TestApplicationEngine.() -> T
+) {
+    val modules = MainModule.plus(module { mockConfiguration() })
+    withTestApplication({ applicationModule(modules) }, test)
+}
+
+inline fun <reified T: Any> Module.factoryMock(crossinline mockConfiguration: (T) -> Unit) {
+    factory(override = true) {
+        mockk<T>().apply { mockConfiguration(this) }
+    }
 }
