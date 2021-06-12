0e5a2896165b66f1f764337f8e2b2aae4ae0319a
Mariusz Marzec <mariusz.marzec00@gmail.com>
2021-06-12 19:02:29 +0200

    Refactoring packages

--- common/src/jvmMain/kotlin/com/marzec/io/ResourceFileReaderImpl.kt
+++
@@ -1,8 +0,0 @@
-package com.marzec.io
-
-class ResourceFileReaderImpl : ResourceFileReader {
-
-    override fun read(fileName: String): String {
-        return Any::class.java.getResource(fileName).readText()
-    }
-}
