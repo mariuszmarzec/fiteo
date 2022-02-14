856eb7379844fda017846c84d86223276cc9b05f
Mariusz Marzec <mariusz.marzec00@gmail.com>
2022-02-14 17:58:01 +0100

    Create common model for transactions

--- src/commonMain/kotlin/com/marzec/trader/model/model.kt
+++ src/commonMain/kotlin/com/marzec/trader/model/model.kt
@@ -1,15 +1,68 @@
 package com.marzec.trader.model
 
 import com.marzec.trader.dto.TransactionDto
+import kotlinx.datetime.LocalDateTime
+
+data class Paper(
+    val id: Long,
+    val code: String,
+    val name: String,
+    val type: PaperType
+)
+
+data class PaperDto(
+    val id: Long,
+    val code: String,
+    val name: String,
+    val type: String
+)
+
+enum class PaperType {
+    CURRENCY,
+    SHARE,
+    COMMODITY
+}
 
 data class Transaction(
     val id: Long,
+    val title: String,
+    val date: LocalDateTime,
+    val targetPaper: Paper,
+    val sourcePaper: Paper,
+    val targetValue: String,
+    val totalPriceInSource: String,
+    val pricePerUnit: String,
+    val rate: String,
+    val fee: String,
+    val feePaper: Paper,
+    val type: TransactionType
 )
 
-fun Transaction.toDto(): TransactionDto = TransactionDto(
-        id = id
+enum class TransactionType {
+    FEE,
+    SALE,
+    PURCHASE,
+    COST
+}
+
+fun Paper.toDto(): PaperDto = PaperDto(
+    id = id,
+    code = code,
+    name = name,
+    type = type.toString(),
 )
 
-fun TransactionDto.toDomain(): Transaction = Transaction(
-            id = id,
-    )
+fun PaperDto.toDomain(): Paper = Paper(
+    id = id,
+    code = code,
+    name = name,
+    type = PaperType.valueOf(type),
+)
+
+//fun Transaction.toDto(): TransactionDto = TransactionDto(
+//    id = id
+//)
+//
+//fun TransactionDto.toDomain(): Transaction = Transaction(
+//    id = id,
+//)
