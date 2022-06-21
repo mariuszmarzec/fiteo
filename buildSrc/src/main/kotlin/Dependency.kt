84eee166bc9ea850383f17ae29982d92ba90c186
Mariusz Marzec <mariusz.marzec00@gmail.com>
2022-06-21 19:48:37 +0200

    Updated kotlin

--- buildSrc/src/main/kotlin/Dependency.kt
+++ buildSrc/src/main/kotlin/Dependency.kt
@@ -1,6 +1,6 @@
 object Dependency {
 
-    val kotlin_version = "1.6.20"
+    val kotlin_version = "1.7.0"
     val detekt_version = "1.18.1"
     val ktor_version = "2.0.2"
     val sl4j_version = "1.6.1"
