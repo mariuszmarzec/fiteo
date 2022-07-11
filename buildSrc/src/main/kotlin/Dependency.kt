f69943062556f827015619bf1dc96b6e913f156d
Mariusz Marzec <mariusz.marzec00@gmail.com>
2022-07-11 13:45:01 +0200

    Updated libraries

--- buildSrc/src/main/kotlin/Dependency.kt
+++ buildSrc/src/main/kotlin/Dependency.kt
@@ -11,9 +11,9 @@ object Dependency {
     val mysql_connector_version = "8.0.29"
     val serialization_version = "1.3.3"
     val datetime_version = "0.2.1"
-    val mockk_version = "1.10.6"
+    val mockk_version = "1.12.4"
     val koin_version = "3.2.0"
     val pre_react_version = "17.0.2-pre.150-kotlin-1.4.31"
     val react_version = "17.0.2"
-    val truth_version = "1.1.2"
+    val truth_version = "1.1.3"
 }
