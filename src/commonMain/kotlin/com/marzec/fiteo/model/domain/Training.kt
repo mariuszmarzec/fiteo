677ba7876c37b586f535c1e6bf87ffd998882db8
Mariusz Marzec <mariusz.marzec00@gmail.com>
2023-04-11 12:23:19 +0200

    Fixed nullability of new field

--- src/commonMain/kotlin/com/marzec/fiteo/model/domain/Training.kt
+++ src/commonMain/kotlin/com/marzec/fiteo/model/domain/Training.kt
@@ -50,7 +50,7 @@ data class UpdateTrainingExerciseWithProgress(
 data class UpdateTrainingExerciseWithProgressDto(
     val exerciseId: Int,
     val series: List<SeriesDto>,
-    val trainingPartId: Int?
+    val trainingPartId: Int? = null
 )
 
 fun UpdateTrainingDto.toDomain() = UpdateTraining(
