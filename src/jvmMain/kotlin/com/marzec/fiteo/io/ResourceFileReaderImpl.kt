d14f5909ab12fbea6ac8e744cd2d1de6b4dbedd9
Mariusz Marzec <mariusz.marzec00@gmail.com>
2023-11-07 23:18:58 +0100

    Fixed reading resources

--- src/jvmMain/kotlin/com/marzec/fiteo/io/ResourceFileReaderImpl.kt
+++ src/jvmMain/kotlin/com/marzec/fiteo/io/ResourceFileReaderImpl.kt
@@ -4,7 +4,7 @@ class ResourceFileReaderImpl : ResourceFileReader {
 
     override fun read(fileName: String): String? {
         return try {
-            Any::class.java.getResource(fileName)!!.readText()
+            ResourceFileReader::class.java.getResource(fileName)!!.readText()
         } catch(ignore: Exception) {
             null
         }
