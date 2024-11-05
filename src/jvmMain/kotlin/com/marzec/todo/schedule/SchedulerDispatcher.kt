cb0009c3098654bf53f69286bc7c39bca3f94dde
Mariusz Marzec <mariusz.marzec00@gmail.com>
2024-11-05 23:11:40 +0100

    Extracted scheduler checker

--- src/jvmMain/kotlin/com/marzec/todo/schedule/SchedulerDispatcher.kt
+++ src/jvmMain/kotlin/com/marzec/todo/schedule/SchedulerDispatcher.kt
@@ -31,10 +31,23 @@ class SchedulerDispatcher(
 
     private val creationTimeFeatureEnabled = true
 
+    private val isInStartWindow: Scheduler.(creationTime: LocalDateTime, today: LocalDateTime) -> Boolean =
+        { creationTime: LocalDateTime, today: LocalDateTime ->
+            val normalisedCreationTime = creationTime.minusHours(timeZoneOffsetHours)
+
+            val intervalEndTime = today
+            val intervalStartTime = intervalEndTime.minusSeconds(schedulerDispatcherInterval / MILLISECONDS_IN_SECOND)
+
+            intervalStartTime <= normalisedCreationTime && normalisedCreationTime <= intervalEndTime
+        }
+
+    private val schedulerChecker = SchedulerChecker(isInStartWindow, creationTimeFeatureEnabled)
+
     fun dispatch() {
         todoRepository.getScheduledTasks().forEach { (user, tasks) ->
             tasks.forEach { task ->
-                if (task.scheduler?.shouldBeCreated() == true) {
+                val today = currentTime().toJavaLocalDateTime()
+                if (task.scheduler != null && schedulerChecker.shouldBeCreated(task.scheduler, today)) {
                     todoService.copyTask(
                         userId = user.id,
                         id = task.id,
@@ -58,134 +71,8 @@ class SchedulerDispatcher(
         }
     }
 
-    private fun Scheduler.shouldBeCreated(): Boolean {
-        return when (this) {
-            is Scheduler.OneShot -> shouldBeCreated()
-            is Scheduler.Monthly -> shouldBeCreated()
-            is Scheduler.Weekly -> shouldBeCreated()
-        }
-    }
-
-    private fun Scheduler.OneShot.shouldBeCreated(): Boolean {
-        val creationTime = startDate.toJavaLocalDateTime()
-            .withHour(hour)
-            .withMinute(minute)
-        return lastDate == null && isInStartWindow(creationTime)
-    }
-
-    private fun Scheduler.Monthly.shouldBeCreated(): Boolean {
-        val today = currentTime().toJavaLocalDateTime()
-        if (today.targetDayOfMonth(dayOfMonth) != today.dayOfMonth) {
-            return false
-        }
-
-        return shouldCreate(
-            calcFirstPeriodDate = { startDate: LocalDate ->
-                val realDayOfMonth =
-                    if (dayOfMonth > startDate.lastDayOfTheMonth()) startDate.lastDayOfTheMonth() else dayOfMonth
-                val startedInNextMonth = startDate.dayOfMonth > realDayOfMonth
-                startDate.withDayOfMonth(realDayOfMonth)
-                    .let { if (startedInNextMonth) it.plusMonths(1) else it }
-            },
-            calcPeriodNumber = { firstPeriodDate, todayLocalDate ->
-                Period.between(firstPeriodDate.plusDays(-1), todayLocalDate).months.inc()
-            }
-        )
-    }
-
-    private fun Scheduler.Weekly.shouldBeCreated(): Boolean {
-        val today = currentTime().toJavaLocalDateTime()
-        if (daysOfWeek.isNotEmpty() && today.dayOfWeek !in daysOfWeek) {
-            return false
-        }
-
-        return shouldCreate(
-            calcFirstPeriodDate = { startDate: LocalDate ->
-                startDate.findFirstDate { it.dayOfWeek in daysOfWeek }
-                    ?.findFirstDate(
-                        mutate = { it.plusDays(-1) },
-                        predicate = { it.dayOfWeek == DayOfWeek.MONDAY }
-                    )
-            },
-            calcPeriodNumber = { firstPeriodDate, todayLocalDate ->
-                val firstPeriodDayOfToday = todayLocalDate.findFirstDate(
-                    mutate = { it.plusDays(-1) },
-                    predicate = { it.dayOfWeek == DayOfWeek.MONDAY }
-                )
-                val daysBetween = Period.between(firstPeriodDate, firstPeriodDayOfToday).days
-                daysBetween / WEEK_DAYS_COUNT + 1
-            }
-        )
-    }
-
-    private fun Scheduler.shouldCreate(
-        calcFirstPeriodDate: (startDate: LocalDate) -> LocalDate?,
-        calcPeriodNumber: (firstPeriodDate: LocalDate, todayLocalDate: LocalDate) -> Int
-    ): Boolean {
-        val today = currentTime().toJavaLocalDateTime()
-        val creationDate = creationDate?.toJavaLocalDateTime()?.takeIf { creationTimeFeatureEnabled } ?: LocalDateTime.MIN
-        val startDateWithHour = startDate.toJavaLocalDateTime().withHour(hour).withMinute(minute)
-        val startDate = if (creationDate > startDateWithHour) {
-            if (creationDate.toLocalDate() == startDateWithHour.toLocalDate()) {
-                creationDate.plusDays(1)
-            } else {
-                creationDate
-            }
-        } else {
-            startDateWithHour
-        }.toLocalDate()
-        val todayLocalDate = today.toLocalDate()
-
-        val firstPeriodDate = calcFirstPeriodDate(startDate) ?: return false
-
-        if (firstPeriodDate <= todayLocalDate) {
-            val periodNumber = calcPeriodNumber(firstPeriodDate, todayLocalDate)
-
-            val isRightPeriod = (periodNumber - 1).mod(repeatInEveryPeriod) == 0
-            val isInCountLimit = repeatCount.takeIf { it > 0 }?.let { maxCount ->
-                (periodNumber - 1) / repeatInEveryPeriod.toFloat() + 1 <= maxCount
-            } ?: true
-
-            if (isRightPeriod && isInCountLimit) {
-                val creationTime = today.withHour(hour).withMinute(minute)
-                return isInStartWindow(creationTime)
-            }
-        }
-        return false
-    }
-
-    private fun LocalDate.findFirstDate(
-        maxDate: LocalDate = currentTime().toJavaLocalDateTime().toLocalDate().plusMonths(3),
-        mutate: (LocalDate) -> LocalDate = { it.plusDays(1) },
-        predicate: (LocalDate) -> Boolean
-    ): LocalDate? =
-        if (predicate(this)) {
-            this
-        } else {
-            val nextDate = mutate(this)
-            if (nextDate <= maxDate) {
-                nextDate.findFirstDate(maxDate, mutate, predicate)
-            } else {
-                null
-            }
-        }
-
-    private fun isInStartWindow(creationTime: LocalDateTime): Boolean {
-        val normalisedCreationTime = creationTime.minusHours(timeZoneOffsetHours)
-
-        val intervalEndTime = currentTime().toJavaLocalDateTime()
-        val intervalStartTime = intervalEndTime.minusSeconds(schedulerDispatcherInterval / MILLISECONDS_IN_SECOND)
-
-        return intervalStartTime <= normalisedCreationTime && normalisedCreationTime <= intervalEndTime
-    }
-
-
     private fun updateLastDate(userId: Int, task: Task) {
         todoService.updateTask(userId, task.id, task.toUpdateWithCurrentLastDate(timeZoneOffsetHours))
-    }
-
-    companion object {
-        private const val WEEK_DAYS_COUNT = 7
     }
 }
 
@@ -215,4 +102,127 @@ private fun LocalDate.lastDayOfTheMonth() = YearMonth.of(year, month).atEndOfMon
 private fun LocalDateTime.targetDayOfMonth(targetDayOfMonth: Int): Int {
     val lastDayOfTheMonth = this.toLocalDate().lastDayOfTheMonth()
     return if (targetDayOfMonth > lastDayOfTheMonth) lastDayOfTheMonth else targetDayOfMonth
+}
+
+private class SchedulerChecker(
+    private val isInStartWindow: Scheduler.(creationTime: LocalDateTime, today: LocalDateTime) -> Boolean,
+    private val creationTimeFeatureEnabled: Boolean
+) {
+    fun shouldBeCreated(scheduler: Scheduler, today: LocalDateTime): Boolean = with(scheduler) {
+        return when (this) {
+            is Scheduler.OneShot -> shouldBeCreated(today)
+            is Scheduler.Monthly -> shouldBeCreated(today)
+            is Scheduler.Weekly -> shouldBeCreated(today)
+        }
+    }
+
+    private fun Scheduler.OneShot.shouldBeCreated(today: LocalDateTime): Boolean {
+        val creationTime = startDate.toJavaLocalDateTime()
+            .withHour(hour)
+            .withMinute(minute)
+        return lastDate == null && isInStartWindow(creationTime, today)
+    }
+
+    private fun Scheduler.Monthly.shouldBeCreated(today: LocalDateTime): Boolean {
+        if (today.targetDayOfMonth(dayOfMonth) != today.dayOfMonth) {
+            return false
+        }
+
+        return shouldCreate(
+            today,
+            calcFirstPeriodDate = { startDate: LocalDate ->
+                val realDayOfMonth =
+                    if (dayOfMonth > startDate.lastDayOfTheMonth()) startDate.lastDayOfTheMonth() else dayOfMonth
+                val startedInNextMonth = startDate.dayOfMonth > realDayOfMonth
+                startDate.withDayOfMonth(realDayOfMonth)
+                    .let { if (startedInNextMonth) it.plusMonths(1) else it }
+            },
+            calcPeriodNumber = { firstPeriodDate, todayLocalDate ->
+                Period.between(firstPeriodDate.plusDays(-1), todayLocalDate).months.inc()
+            }
+        )
+    }
+
+    private fun Scheduler.Weekly.shouldBeCreated(today: LocalDateTime): Boolean {
+        if (daysOfWeek.isNotEmpty() && today.dayOfWeek !in daysOfWeek) {
+            return false
+        }
+
+        return shouldCreate(
+            today,
+            calcFirstPeriodDate = { startDate: LocalDate ->
+                startDate.findFirstDate { it.dayOfWeek in daysOfWeek }
+                    ?.findFirstDate(
+                        mutate = { it.plusDays(-1) },
+                        predicate = { it.dayOfWeek == DayOfWeek.MONDAY }
+                    )
+            },
+            calcPeriodNumber = { firstPeriodDate, todayLocalDate ->
+                val firstPeriodDayOfToday = todayLocalDate.findFirstDate(
+                    mutate = { it.plusDays(-1) },
+                    predicate = { it.dayOfWeek == DayOfWeek.MONDAY }
+                )
+                val daysBetween = Period.between(firstPeriodDate, firstPeriodDayOfToday).days
+                daysBetween / WEEK_DAYS_COUNT + 1
+            }
+        )
+    }
+
+    private fun Scheduler.shouldCreate(
+        today: LocalDateTime,
+        calcFirstPeriodDate: (startDate: LocalDate) -> LocalDate?,
+        calcPeriodNumber: (firstPeriodDate: LocalDate, todayLocalDate: LocalDate) -> Int
+    ): Boolean {
+        val creationDate =
+            creationDate?.toJavaLocalDateTime()?.takeIf { creationTimeFeatureEnabled } ?: LocalDateTime.MIN
+        val startDateWithHour = startDate.toJavaLocalDateTime().withHour(hour).withMinute(minute)
+        val startDate = if (creationDate > startDateWithHour) {
+            if (creationDate.toLocalDate() == startDateWithHour.toLocalDate()) {
+                creationDate.plusDays(1)
+            } else {
+                creationDate
+            }
+        } else {
+            startDateWithHour
+        }.toLocalDate()
+        val todayLocalDate = today.toLocalDate()
+
+        val firstPeriodDate = calcFirstPeriodDate(startDate) ?: return false
+
+        if (firstPeriodDate <= todayLocalDate) {
+            val periodNumber = calcPeriodNumber(firstPeriodDate, todayLocalDate)
+
+            val isRightPeriod = (periodNumber - 1).mod(repeatInEveryPeriod) == 0
+            val isInCountLimit = repeatCount.takeIf { it > 0 }?.let { maxCount ->
+                (periodNumber - 1) / repeatInEveryPeriod.toFloat() + 1 <= maxCount
+            } ?: true
+
+            if (isRightPeriod && isInCountLimit) {
+                val creationTime = today.withHour(hour).withMinute(minute)
+                return isInStartWindow(creationTime, today)
+            }
+        }
+        return false
+    }
+
+    private fun LocalDate.findFirstDate(
+        maxDate: LocalDate = currentTime().toJavaLocalDateTime().toLocalDate().plusMonths(3),
+        mutate: (LocalDate) -> LocalDate = { it.plusDays(1) },
+        predicate: (LocalDate) -> Boolean
+    ): LocalDate? =
+        if (predicate(this)) {
+            this
+        } else {
+            val nextDate = mutate(this)
+            if (nextDate <= maxDate) {
+                nextDate.findFirstDate(maxDate, mutate, predicate)
+            } else {
+                null
+            }
+        }
+
+
+    companion object {
+        private const val WEEK_DAYS_COUNT = 7
+    }
 }
